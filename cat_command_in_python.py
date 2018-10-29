84f88a8a331e02dbd748d4f93ebe078c594d0fb6
Rahu Noulia <36097160+RahulNoulia@users.noreply.github.com>
2018-10-29 17:50:33 +0530

    Add files via upload

---
+++ cat_command_in_python.py
@@ -0,0 +1,33 @@
+from sys import argv
+import os
+
+
+def read_file(fname):
+
+    try:
+        with open(fname,"r") as fd:
+            for line in fd:
+                yield line
+    except:
+        print('some error occurred')
+
+  # Main function
+
+def main():
+
+    if len(argv) != 2:
+        print('Usage of Script  ->  python3.6 cat.py <file_path> ')
+        return
+
+    _, fname = argv
+
+    if not os.path.isfile(fname):
+        print(f'No such file: {fname} ')
+        return
+
+    for line in read_file(fname):
+        print(line)
+
+
+if __name__ == '__main__':
+    main()
